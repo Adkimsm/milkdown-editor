71f8e1ddbd99a02f81175d4d26c06a497ef7b574
adk <75924586+Adkimsm@users.noreply.github.com>
2023-08-13 12:53:53 +0000

    feat: prism dark style

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './App.css'
+import React, { useEffect } from 'react';
+import './App.css';
 import { defaultValueCtx, Editor, rootCtx } from '@milkdown/core';
 import { nord } from '@milkdown/theme-nord';
 import { Milkdown, MilkdownProvider, useEditor } from '@milkdown/react';
@@ -10,29 +10,34 @@ import { prism } from '@milkdown/plugin-prism';
 import { math } from '@milkdown/plugin-math';
 import { emoji } from '@milkdown/plugin-emoji';
 
-import '@milkdown/theme-nord/style.css'
+import '@milkdown/theme-nord/style.css';
 
-const markdown =
-`# Milkdown React Commonmark
+const markdown = `# Milkdown React Commonmark
 
 > You're scared of a world where you're needed.
 
-This is a demo for using Milkdown with **React**.`
+This is a demo for using Milkdown with **React**.
+
+\`\`\`sh
+sudo pacman -Syyu
+\`\`\`
+
+`;
 
 const MilkdownEditor = () => {
-    const { get } = useEditor((root) =>
-      Editor.make()
-        .config(nord)
-        .config((ctx) => {
-          ctx.set(rootCtx, root);
-          ctx.set(defaultValueCtx, markdown)
-        })
-        .use(commonmark)
-        .use(gfm)
-        .use(clipboard)
-        .use(prism)
-        .use(emoji)
-        .use(math),
+  const { get } = useEditor((root) =>
+    Editor.make()
+      .config(nord)
+      .config((ctx) => {
+        ctx.set(rootCtx, root);
+        ctx.set(defaultValueCtx, markdown);
+      })
+      .use(commonmark)
+      .use(gfm)
+      .use(clipboard)
+      .use(prism)
+      .use(emoji)
+      .use(math)
   );
 
   return <Milkdown />;
